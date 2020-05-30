f2a77188173ee2b1185a667e1efb13edb1dfbfb4
rockstar2514 <56267835+rockstar2514@users.noreply.github.com>
2020-05-30 13:44:57 +0530

    Interactive :/

--- Assignment 6 Trial/Client.java
+++ Assignment 6 Trial/Client.java
@@ -16,44 +16,39 @@ public class Client {
     	 Scanner sc=new Scanner(System.in);
     	 System.out.println("Enter Main URL");
     	 String url=sc.nextLine();
-//    	 HttpClient client = HttpClient.newBuilder()
-//    		        .version(Version.HTTP_1_1)
-//    		        .followRedirects(Redirect.NORMAL)
-//    		        .connectTimeout(Duration.ofSeconds(20))
-//    		        .authenticator(Authenticator.getDefault())
-//    		        .build();
-//    	  HttpRequest request = HttpRequest.newBuilder()
-//    		         .uri(URI.create("http://foo.com/"))
-//    		         .build();
-//    	 HttpClient client = HttpClient.newHttpClient();
-//         HttpRequest request = HttpRequest.newBuilder()
-//                 .uri(URI.create(url+"//add"))
-//                 .build();
-//
-//         HttpResponse<String> response = client.send(request,
-//                 HttpResponse.BodyHandlers.ofString());
-//
-//         System.out.println(response.body());
+       if(url.charAt(url.length()-1)!='/')url=url+'/';   
     	  HttpClient client = HttpClient.newHttpClient();
-          HttpRequest request = HttpRequest.newBuilder()
-                  .uri(URI.create(url+"add"))
-                  .POST(HttpRequest.BodyPublishers.ofFile(Paths.get("json1.json")))
-                  .build();
-
-          HttpResponse<String> response = client.send(request,
-                  HttpResponse.BodyHandlers.ofString());
-
-          System.out.println(response.body());
-          
-        // HttpClient client2 = HttpClient.newHttpClient();
-         HttpRequest request2 = HttpRequest.newBuilder()
-                 .uri(URI.create(url+"list"))
-                 .build();
-
-         HttpResponse<String> response2 = client.send(request2,
-                 HttpResponse.BodyHandlers.ofString());
-
-         System.out.println(response2.body());
-    	 
+         int flag=1;
+         do
+         {
+        	 System.out.println("Enter 1 to add data and enter 2 to list data and any other button to end");
+        	 int choice=Integer.parseInt(sc.nextLine());
+        	 switch(choice)
+        	 {
+        	     case 1:
+        	    	 System.out.println("Enter file directory");
+        	    	 String dir=sc.nextLine();
+        	    	 HttpRequest request = HttpRequest.newBuilder()
+                     .uri(URI.create(url+"add"))
+                     .POST(HttpRequest.BodyPublishers.ofFile(Paths.get(dir)))
+                     .build();
+        	    	 HttpResponse<String> response = client.send(request,
+        	                  HttpResponse.BodyHandlers.ofString());
+        	          System.out.println(response.body());
+        	          break;
+        	     case 2:
+        	    	 HttpRequest request2 = HttpRequest.newBuilder()
+                     .uri(URI.create(url+"list"))
+                     .build();
+		             HttpResponse<String> response2 = client.send(request2,
+		                     HttpResponse.BodyHandlers.ofString());
+		             System.out.println(response2.body());
+		             break;
+                  default:
+                	  flag=0;
+                	  break;
+        	 }
+         }
+         while(flag==1);
      }
 }
