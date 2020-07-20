1d4adf15e3a5ab9feb97649ed6959af1f3dc75e5
rockstar2514 <dawoodbinmansoor@gmail.com>
2020-07-20 10:53:15 +0530

    Deleted Files
    Some files were deleted that were unnecesary

--- Mining.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-s=input()
-result = hashlib.sha256(s.encode()) 
-for i in range (65536000):
-    curr=s+str(i)
-    newres=hashlib.sha256(curr.encode())
-    ans=newres.hexdigest()
-    if(int(ans,16)<0x0000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF):
-        print (curr)
